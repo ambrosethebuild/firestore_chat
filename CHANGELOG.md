121e64649e685a1f842ae87ef7ad825606f40194
ambrosethebuild <bakoambrose@gmail.com>
2022-12-16 05:52:52 +0000

    fix: dark mode message input color

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 0.0.3
+
+* Fix dark mode message input color
 ## 0.0.2
 
 * Add example and fix name issue
--- lib/views/firestore_chat.page.dart
+++ lib/views/firestore_chat.page.dart
@@ -12,39 +12,42 @@ class FirestoreChatPage extends StatelessWidget {
   //
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(chatEntity.title ?? ""),
-      ),
-      body: ViewModelBuilder<FirestoreChatViewModel>.reactive(
-        viewModelBuilder: () => FirestoreChatViewModel(context, chatEntity),
-        onModelReady: (model) => model.initialise(),
-        builder: (context, vm, child) {
-          return DashChat(
-            currentUser: chatEntity.mainUser.toChatUser(),
-            onSend: vm.sendMessage,
-
-            messages: vm.messages,
-            // inputOptions: const InputOptions(
-            //   trailing: [
-            //     Padding(
-            //       padding: EdgeInsets.all(8.0),
-            //       child: Icon(
-            //         Ionicons.image,
-            //       ),
-            //     ),
-            //   ],
-            // ),
-            messageListOptions: MessageListOptions(
-              showDateSeparator: false,
-              chatFooterBuilder: vm.isBusy
-                  ? const Center(
-                      child: CircularProgressIndicator(strokeWidth: 2),
-                    )
-                  : const SizedBox.shrink(),
-            ),
-          );
-        },
+    return Theme(
+      data: Theme.of(context).copyWith(brightness: Brightness.dark),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(chatEntity.title ?? ""),
+          // backgroundColor: Theme.of(context).primaryColor,
+        ),
+        body: ViewModelBuilder<FirestoreChatViewModel>.reactive(
+          viewModelBuilder: () => FirestoreChatViewModel(context, chatEntity),
+          onModelReady: (model) => model.initialise(),
+          builder: (context, vm, child) {
+            return Theme(
+              data: Theme.of(context),
+              child: DashChat(
+                currentUser: chatEntity.mainUser.toChatUser(),
+                onSend: vm.sendMessage,
+                messages: vm.messages,
+                inputOptions: InputOptions(
+                  inputTextStyle: TextStyle(
+                    color: Theme.of(context).brightness != Brightness.dark
+                        ? Colors.white
+                        : Colors.black,
+                  ),
+                ),
+                messageListOptions: MessageListOptions(
+                  showDateSeparator: false,
+                  chatFooterBuilder: vm.isBusy
+                      ? const Center(
+                          child: CircularProgressIndicator(strokeWidth: 2),
+                        )
+                      : const SizedBox.shrink(),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
